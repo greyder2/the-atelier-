e7d10b5bc2b927a283161d5184aaeef8279786fa
ibrahim <greyder22@gmail.com>
2026-04-22 01:37:15 -0600

    fix: spotlights variable build error in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,12 @@ import Link from 'next/link';
 export const dynamic = 'force-dynamic';
 
 export default async function Home() {
+    const spotlights = await client.fetch(`*[_type == "spotlight"]{
+        name,
+        "slug": slug.current,
+        category
+    }`);
+
     return (
         <main>
             <InteractiveClient />
